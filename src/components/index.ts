699dcd0b4645d78b2ed5efbc6cf7abe2e470dcc1
Jonatas <jonatasf1997@hotmail.com>
2022-02-12 14:26:07 -0300

    feat: Keyframs and some configurations add

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as ToTopButton } from './ToTopButton';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as Intersection } from './Intersection';
+export { default as SimpleParallax } from './SimpleParallax';
+export { default as LoadComponent } from './Loadings/LoadComponent';
+export { default as LoadingCircle } from './Loadings/LoadingCircle';
---
+++ src/styles/keyframs.ts
@@ -0,0 +1,37 @@
+import { keyframes } from 'styled-components';
+
+export const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }to{
+    transform: rotate(360deg);
+  }
+`;
+
+export const appearFromRight = keyframes`
+  from{
+    opacity: 0;
+    transform: translateX(-50px);
+  }to{
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+export const appearFromNothing = keyframes`
+  from{
+    opacity: 0;
+  }to{
+    opacity: 1;
+  }
+`;
+
+export const appearFromTop = keyframes`
+  from{
+    opacity: 0;
+    transform: translateY(-50px);
+  }to{
+    opacity: 1;
+    transform: translateY(0px);
+  }
+`;
