fb681fa126b977578a2f3ab299fe7ba2e9c9ea7a
Jonatas <jonatasf1997@hotmail.com>
2022-02-14 13:38:00 -0300

    alterado arquitetura de pastas

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,18 @@ import commonjs from '@rollup/plugin-commonjs';
 import url from '@rollup/plugin-url';
 import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
 
 export default [
   {
     input: 'src/lib/components/index.ts',
     output: [
       {
-        name: 'Components',
-        file: './dist/components/index.js',
+        file: './components/index.js',
         format: 'esm',
       },
       {
-        name: 'Components',
-        file: './dist/components/index.cjs.js',
+        file: './components/index.cjs.js',
         format: 'cjs',
       },
     ],
@@ -23,30 +22,44 @@ export default [
       commonjs(),
       url(),
       nodeResolve(),
-      typescript({ tsconfig: './tsconfig.json' }),
       babel({
         exclude: 'node_modules/**',
       }),
+      typescript(),
     ],
     external: ['react', 'react-dom', 'styled-components'],
   },
   {
+    input: './src/lib/components/index.ts',
+    output: [{ file: 'components/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+  {
     input: 'src/lib/functions/index.ts',
-    output: {
-      name: 'Functions',
-      file: './dist/functions/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
+    output: [
+      {
+        file: './functions/index.js',
+        format: 'esm',
+      },
+      {
+        file: './functions/index.cjs.js',
+        format: 'cjs',
+      },
+    ],
     plugins: [
       commonjs(),
       url(),
       nodeResolve(),
-      typescript({ tsconfig: './tsconfig.json' }),
       babel({
         exclude: 'node_modules/**',
       }),
+      typescript({}),
     ],
     external: ['react', 'react-dom', 'styled-components'],
+  },
+  {
+    input: './src/lib/functions/index.ts',
+    output: [{ file: 'functions/index.d.ts', format: 'es' }],
+    plugins: [dts()],
   },
 ];
