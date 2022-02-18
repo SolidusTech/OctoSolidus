ae9b3dc21bfb87d778e4c3d88e782da3f51d8eda
Jonatas <jonatasf1997@hotmail.com>
2022-02-18 13:55:54 -0300

    feat: add new pages

---
+++ docs/src/pages/hooks/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Hooks: NextPage = () => {
+  return <div>Hooks</div>;
+};
+
+export default Hooks;
---
+++ docs/src/pages/styles/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Styles: NextPage = () => {
+  return <div>Styles</div>;
+};
+
+export default Styles;
